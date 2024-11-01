1bacb2df8399cc73482d8ab30205e366c8afa12e
MD IMRAN HOSSAIN <153748024+imranjewel15@users.noreply.github.com>
2024-11-01 22:38:08 +0600

    Add files via upload
    Condition Changed

--- script.js
+++ script.js
@@ -11,41 +11,33 @@ let playerTwo = document.querySelector("#playerTwo");
 let playerOne = document.querySelector("#playerOne");
 let snowFall = document.querySelector("#snowFall");
 
-btn.addEventListener("click", ()=>{
-    if(!inputOne.value){
-        errorMsg.innerHTML ="You do not enter any number.Please Input a number "
-    }
-    if(inputOne.value>9 ||inputOne.value<0){
-        errorMsg.innerHTML ="Invalid Number.Please Input Number 0 to 9"
-    }
-    else{
-        errorMsg.innerHTML ="";
-        console.log(inputOne.value);
-        fullWrapper.classList.add("flip");
-    }
-})
+btn.addEventListener("click", () => {
+  // if(!inputOne.value){
+  //     errorMsg.innerHTML ="You do not enter any number.Please Input a number "
+  // }
+  if (!inputOne.value || inputOne.value > 9 || inputOne.value < 0) {
+    errorMsg.innerHTML = "Invalid Number.Please Input Number 0 to 9";
+  } else {
+    errorMsg.innerHTML = "";
+    fullWrapper.classList.add("flip");
+  }
+});
 
-btnTwo.addEventListener("click", ()=>{
-    
-    if(!inputTwo.value){
-        errorMsgTwo.innerHTML ="You do not enter any number.Please Input a number "
+btnTwo.addEventListener("click", () => {
+  // if(!inputTwo.value){
+  //     errorMsgTwo.innerHTML ="You do not enter any number.Please Input a number "
+  // }
+  if (!inputTwo.value || inputTwo.value > 9 || inputTwo.value < 0) {
+    errorMsgTwo.innerHTML = "Invalid Number.Please Input Number 0 to 9";
+  } else {
+    playerOne.style.display = "none";
+    playerTwo.style.display = "none";
+    winner.style.transform = "rotateY(180deg)";
+    winner.style.display = "block";
+    if (inputOne.value == inputTwo.value) {
+      result.innerHTML = "PLAYER - 2";
+    } else {
+      result.innerHTML = "PLAYER - 1";
     }
-    if(inputTwo.value>9 ||inputTwo.value<0){
-        errorMsgTwo.innerHTML ="Invalid Number.Please Input Number 0 to 9"
-    }
-    else{
-        playerOne.style.display = "none"; 
-        playerTwo.style.display = "none";
-        winner.style.transform = "rotateY(180deg)"
-        winner.style.display = "block";
-        if(inputOne.value == inputTwo.value){
-            
-            result.innerHTML="PLAYER - 2"
-        }
-        else{ 
-            result.innerHTML="PLAYER - 1"
-        }
-
-    }
-})
-
+  }
+});
